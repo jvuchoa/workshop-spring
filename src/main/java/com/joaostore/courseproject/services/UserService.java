8e755a186758b3c1cca5201cc337684b27f062e5
João Uchoa <joaovictoruchoa01@gmail.com>
2025-07-11 13:08:08 -0300

    Service layer, component registration

---
+++ src/main/java/com/joaostore/courseproject/services/UserService.java
@@ -0,0 +1,24 @@
+package com.joaostore.courseproject.services;
+
+import com.joaostore.courseproject.entities.User;
+import com.joaostore.courseproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
